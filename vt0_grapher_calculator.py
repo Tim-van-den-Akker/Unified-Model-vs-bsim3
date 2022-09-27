046fa1c2563b9c423440bba48747cadb4beb18b5
Tim van den Akker <timakk01@gmail.com>
2022-09-27 14:30:42 +0200

    added sqrt(Id) and D(sqrt(Id))

---
+++ vt0_grapher_calculator.py
@@ -0,0 +1,61 @@
+from itertools import count
+from sqlite3 import Row
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from math import sqrt
+
+data = pd.read_excel('sqrtId_DId.xlsx')
+print(data)
+IdArr = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+DIdArr = np.array([0, 0, 0, 0, 0, 0, 0, 0])
+
+length = 901
+i = 1
+
+spiceName1 = "sqrt(Id(M1))"
+spiceName2 = "D(sqrt(Id(M1)))"
+
+while i < length:
+    row = np.array([data["vgs"][i+1], data[spiceName1][1 + i], data[spiceName1][1 + i + (length+1)], data[spiceName1][1 + i + 2*(length+1)], data[spiceName1][1 + i + 3*(length+1)], data[spiceName1][1 + i + 4*(length+1)], data[spiceName1][1 + i + 5*(length+1)], data[spiceName1][1 + i + 6*(length+1)]])
+    IdArr = np.vstack([IdArr, row])
+    i += 1
+
+i = 1
+while i < length:
+    row = np.array([data["vgs"][i+1], data[spiceName2][1 + i], data[spiceName2][1 + i + (length+1)], data[spiceName2][1 + i + 2*(length+1)], data[spiceName2][1 + i + 3*(length+1)], data[spiceName2][1 + i + 4*(length+1)], data[spiceName2][1 + i + 5*(length+1)], data[spiceName2][1 + i + 5*(length+1)]])
+    DIdArr = np.vstack([DIdArr, row])
+    i += 1
+
+
+plot = IdArr.transpose()
+plot = plot * 1000
+plot[0] = plot[0] / 1000
+
+plotu = DIdArr.transpose()
+plotu = plotu * 1000
+plotu[0] = plotu[0] / 1000
+
+# plt.plot (plot[0], plot[1], label='0V')
+plt.plot (plot[0], plot[2], label='0.3V')
+plt.plot (plot[0], plot[3], label='0.6V')
+plt.plot (plot[0], plot[4], label='0.9V')
+plt.plot (plot[0], plot[5], label='1.2V')
+plt.plot (plot[0], plot[6], label='1.5V')
+plt.plot (plot[0], plot[7], label='1.8V')
+
+# plt.plot (plotu[0], plotu[1],'--', label='0V')
+plt.plot (plotu[0], plotu[2],'--', label='0.3V')
+plt.plot (plotu[0], plotu[3],'--', label='0.6V')
+plt.plot (plotu[0], plotu[4],'--', label='0.9V')
+plt.plot (plotu[0], plotu[5],'--', label='1.2V')
+plt.plot (plotu[0], plotu[6],'--', label='1.5V')
+plt.plot (plotu[0], plotu[7],'--', label='1.8V')
+plt.legend(prop={'size' : 9})
+plt.title('$I_d$ against $V_{ds}$ simulated Unified Model vs BSIM3 simulation')
+plt.xlabel('$V_{gs}$ (V)')
+plt.ylabel('$\sqrt{I_d}$ ($\sqrt{mA}$)')
+plt.savefig("D(sqrt(Id))_sqrt(Id)", dpi=600)
+# plt.show()
+exit()
+
